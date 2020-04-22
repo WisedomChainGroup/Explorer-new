9494396dd1772361a3d8912763859394edcb701c
570749280 <570749280r@qq.com>
2020-04-22 16:08:16 +0800

    新版更新资产页面

---
+++ cn/js/blockList.js
@@ -0,0 +1,85 @@
+$(function() {	
+		
+	var height = GetQueryString("height");
+	if (height != undefined &&
+		height != null &&
+		height != "undefined" &&
+		height != "null" &&
+		height != "") {
+			blockListPlay(height>=1?height:1);
+	} else {
+		window.location.href='block.html';
+	}
+	$('#div_transaction').click(function(){
+		$('.box').slideToggle();
+		$('#table_showdiv').css('visibility','visible');
+	});
+	$('#table_showdiv').click(function(){
+		$('.box').slideToggle();
+		$('#table_showdiv').css('visibility','hidden');
+	});
+}); 
+
+function blockListPlay(height=1){
+	$.get(HttpBlockHead+"/block/detail?height="+height,
+			function(result) {
+				setHtml(result.data, 'tpl3', 'blocks_data_List');
+			}); 
+			
+    $.get(HttpBlockHead+"/transactions/list?height="+height,
+    		function(result) {
+    			if (result.code == "200") {
+					for (var i = 0; i < result.data.length; i++) {
+						//hash处理
+						result.data[i].hash = result.data[i].transactionhash;
+						var transactionhash = result.data[i].transactionhash.substring(0, 5) + "***" + result.data[i].transactionhash.substring(result.data[i].transactionhash.length - 5, result.data[i].transactionhash.length);
+						result.data[i].transactionhash = transactionhash;
+						//事务类型处理
+						var type =  result.data[i].type;
+						switch (type){
+							case '0':
+								result.data[i].type='CoinBase';
+								result.data[i].fromaddress='';
+								break;
+							case '1':
+								result.data[i].type='转账';
+								break;
+							case '2':
+								result.data[i].type='发起投票';
+								break;
+							case '3':
+								result.data[i].type='存证';
+								break;
+							case '9':
+								result.data[i].type='申请孵化';
+								break;
+							case '10':
+								result.data[i].type='提取利息';
+								break;
+							case '11':
+								result.data[i].type='分享收益';
+								break;
+							case '12':
+								result.data[i].type='提取本金';
+								break;
+							case '13':
+								result.data[i].type='撤回投票';
+								break;
+							case '14':
+								result.data[i].type='抵押';
+								break;
+							case '15':
+								result.data[i].type='撤回抵押';
+								break;
+							default:
+								break;
+						}
+						result.data[i].transactionhash = transactionhash;
+						result.data[i].amount = result.data[i].amount/100000000;
+					}
+					console.log(result.data);
+					setHtml(result.data, 'tpl4', 'transactions_data_List');
+				}
+    		}); 
+}
+
---
+++ cn/js/blockverify.js
@@ -0,0 +1,46 @@
+/**
+ * 根据事务hash获取存证信息
+ * @param {Object} hash
+ */
+function userTransferLog(hash) {
+	//var hash = "undefined" ? "" : hash;
+	if ( hash== "") {
+		//alert();
+		$("#content").html("未查到该信息");
+		return;
+	} 
+	//GET /userTransferLog/getTransaction
+	//数据请求部分
+	$.post(HttpHead + "/userTransferLog/getTransaction/", {
+		coinHash: hash,
+	},
+	function(result) {
+		
+		console.log(result.data);
+		
+		if(result.code=="2000"){
+			//result.data.fee = result.data.fee*0.00000001
+			setHtml(result.data,'tpl','content');
+		}
+		
+	});
+
+}
+
+
+var GetQueryString_hash = GetQueryString("hash");
+
+if(GetQueryString_hash != undefined && 
+	GetQueryString_hash != null && 
+	GetQueryString_hash != "undefined" && 
+	GetQueryString_hash != "null" && 
+	GetQueryString_hash != "") {
+		$("#soso").val(GetQueryString_hash);
+		$("#sosowap").val(GetQueryString_hash);
+		//alert(GetQueryString_hash);
+	//localStorage.hash = GetQueryString_hash;
+	//getDetailInfo(localStorage.biMarker);
+	userTransferLog(GetQueryString_hash);
+	//userTransferLog("83ed8ce2f67cc3e05543d637ddd708ba2691dcd827c54e38a7e37a522739c60f");
+}
+
