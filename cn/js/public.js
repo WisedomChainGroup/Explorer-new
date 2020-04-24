61b087b2a9e9193d6abf732bfbf09b59571b0437
570749280 <570749280r@qq.com>
2020-04-24 14:40:39 +0800

    页面优化

--- cn/js/public.js
+++ cn/js/public.js
@@ -5,19 +5,19 @@
 //var HttpHead="https://wdcwallet.hkcyb.pro/wisechain";  //正式
 //var HttpBlockHead="http://localhost:8080";//测试
 //var HttpBlock="http://47.96.67.155:19585"; //节点请求地址
-<<<<<<< HEAD
+
 var HttpBlockHead = "http://192.168.1.83:8081";
 
 var HttpHead = "http://192.168.1.83:9090/wisechain";
-=======
+
 //var HttpHead = "http://192.168.1.83:9090/wisechain";
 
  // var HttpBlockHead = "http://192.168.1.83:8081";
  // var HttpHead = "http://192.168.1.83:9090/wisechain";
 
- var HttpHead="https://wdcwallet.hkcyb.pro/wisechain";  //正式
-var HttpBlockHead = "https://scannode.hkcyb.pro"; //正式
->>>>>>> origin/develop
+// var HttpHead="https://wdcwallet.hkcyb.pro/wisechain";  //正式
+//var HttpBlockHead = "https://scannode.hkcyb.pro"; //正式
+
 
 /**
  * 设置模板宣言
