5e048f7b465d959dafff20722b5e9cd579b07b3f
570749280 <570749280r@qq.com>
2020-04-26 16:10:00 +0800

    分页添加选项

--- js/public.js
+++ js/public.js
@@ -1,5 +1,5 @@
 // var HttpHead="https://wdcwallet.hkcyb.pro/wisechain";  //正式
-//var HttpBlockHead = "https://scannode.hkcyb.pro"; //正式
+// var HttpBlockHead = "https://scannode.hkcyb.pro"; //正式
 //var HttpBlockHead="http://192.168.1.67:8080";//测试
 //var HttpBlock="http://47.96.67.155:19585"; //节点请求地址
 
@@ -7,7 +7,6 @@
 //	测试线上
 //var HttpHead = "http://47.52.195.58:9090/wisechain";
 //正式
-//var HttpBlockHead = "https://scannode.hkcyb.pro";
 var HttpBlockHead = "http://192.168.1.83:8081";
 var HttpHead = "http://192.168.1.83:9090/wisechain";
 
