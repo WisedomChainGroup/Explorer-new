28911a06f636823ecac643765762716e24854733
570749280 <570749280@qq.com>
2020-05-25 10:25:44 +0800

    手机版导航优化

---
+++ cn/js/navbber.js
@@ -0,0 +1,15 @@
+$(document).ready(function(e) {
+	$("#h-arrow").click(function(e) {
+		if( $("#h-btnshow").hasClass("h-mobileMenuLanguage___3lWvP") ){
+	// 执行隐藏
+			$("#h-btnshow").hide().removeClass("h-mobileMenuLanguage___3lWvP");
+			document.getElementById("h-arrow-down").style.display = "none";
+			document.getElementById("h-arrow-right").style.display = "block";
+		}else{
+			// 显示
+			$("#h-btnshow").show().addClass("h-mobileMenuLanguage___3lWvP");
+			document.getElementById("h-arrow-down").style.display = "block";
+			document.getElementById("h-arrow-right").style.display = "none";
+		}
+	 });
+});
